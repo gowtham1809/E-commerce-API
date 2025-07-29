ef968b6fcacb0a914fbbe89e31d01fce366809e4
gowthamraj1809 <gowthamrajmamundi2002.com>
2025-07-29 16:54:57 +0530

    Update .gitignore, improve dotenv configuration, and enhance API tests

--- src/index.js
+++ src/index.js
@@ -1,34 +1,33 @@
-require("dotenv").config();
+const env = process.env.NODE_ENV || "development";
+require("dotenv").config({ path: `.env.${env}` });
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors')
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/devdb";
+const Frontend_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: Frontend_URL,
     credentials: true,
   })
 );
 app.use(express.json());
 app.use(cookieParser());
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => console.log("MongoDB Connected"))
-//   .catch((err) => console.log(err));
 
-// const mainRoutes = require('./routes/mainRoutes');
+ mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
-app.get('/api', (req, res) => {
-  res.status(200).json({ message: 'Welcome to the API' });
+const mainRoutes = require("./routes/mainRoutes");
+
+app.use("/api", mainRoutes);
+const PORT = process.env.PORT || 50;
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
-// app.use('/api', mainRoutes);
-const PORT = process.env.PORT || 5000;
- const server =app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    
- })
 
 module.exports = { app, server };
--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,15 +1,16 @@
 const request = require("supertest");
+const mongoose = require("mongoose");
 const { app, server } = require("../index");
 
 describe("API Tests", () => {
   it("should respond with a welcome message", async () => {
-    const response = await request(app).get("/api");
+    const response = await request(app).get("/api/users");
     expect(response.statusCode).toBe(200);
-    expect(response.body.message).toBe("Welcome to the API");
   });
 
   afterAll(async () => {
     await server.close();
-    console.log("Server closed");
+    await mongoose.connection.close();
+    console.log("Server and DB connection closed");
   });
 });
