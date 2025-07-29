02be5fdbbe333eaf603dd92592eab74c204e1062
gowthamraj1809 <gowthamrajmamundi2002.com>
2025-07-29 17:12:54 +0530

    Add MongoDB service to CI pipeline and fix default port in server configuration

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 const mainRoutes = require("./routes/mainRoutes");
 
 app.use("/api", mainRoutes);
-const PORT = process.env.PORT || 50;
+const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
